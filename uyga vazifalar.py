2a92dfbd799292426e4f0f4158478c019073799d
otabekruzmetov <148218174+otabekruzmetov@users.noreply.github.com>
2023-10-26 00:27:54 +0500

    Ro'zmetov Otabek

--- uyga vazifalar.py
+++ uyga vazifalar.py
@@ -1,15 +1,15 @@
-# 'UY ISHI'
-# '16-DARS FUNKSIYA'
-# ('uyga vazifa sharti:"1.Foydalanuvchidan son olib, '
-#  'uning kvadrati va kubini '
-#  'konsolga chiqaruvchi funksiya yozing.')
-#
-# def Foydalanuvchi():
-#     """Kvadrat va kubni konsolga chiqaruvchi funksiya"""
-#     print(Foydalanuvchi.__doc__)
-#     square = int(input("Hohlagan natural son yozing>>"))
-#     print(f"{square} ning kavadrati, = {square,  2} ga teng")
-#     print(f"{square} ning kubi, = {square,  3} ga teng")
+'UY ISHI'
+'16-DARS FUNKSIYA'
+('uyga vazifa sharti:"1.Foydalanuvchidan son olib, '
+ 'uning kvadrati va kubini '
+ 'konsolga chiqaruvchi funksiya yozing.')
+
+def Foydalanuvchi():
+    """Kvadrat va kubni konsolga chiqaruvchi funksiya"""
+    print(Foydalanuvchi.__doc__)
+    square = int(input("Hohlagan natural son yozing>>"))
+    print(f"{square} ning kavadrati, = {square,  2} ga teng")
+    print(f"{square} ning kubi, = {square,  3} ga teng")
 
 'UY ISHI'
 "18-DARS FUNKSIYA VA RO'HAT"
