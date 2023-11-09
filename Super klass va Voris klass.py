c09022e3b7e0a35710a399172033b88ca9671860
otabekruzmetov <148218174+otabekruzmetov@users.noreply.github.com>
2023-11-09 15:40:28 +0500

    otabek

--- Super klass va Voris klass.py
+++ Super klass va Voris klass.py
@@ -1,6 +1,75 @@
+# # class Shaxs:
+# #     """Sahxslarni haqida ma'lumot"""
+# #     def __init__(self,ism,familiya,passport,tyil):
+# #         self.ism = ism
+# #         self.familiya = familiya
+# #         self.passport = passport
+# #         self.tyil = tyil
+# #
+# #     def get_info(self):
+# #         """Shaxs haqida ma'lumot"""
+# #         info = f"{self.ism} {self.familiya}."
+# #         info += f"Passport.{self.passport},{self.tyil}-yilda tug'ilgan"
+# #         return info
+# #
+# #     def get_age(self,yil):
+# #         """Shaxsning yoshini qaytaruvchi metod"""
+# #         return yil - self.tyil
+# #
+# # # inson = Shaxs("Otabek","Ro'zmetov","FB1122",2008)
+# # # print(f"{inson.get_info()}. {inson.get_age(2023)} yoshda")
+# #
+# # class Talaba(Shaxs):
+# #     """talaba Klassi"""
+# #     def __init__(self,ism,familiya,passport,tyil,idraqam,manzil):
+# #         """Talabaning xususiyatlari"""
+# #         super().__init__(ism,familiya,passport,tyil)
+# #         self.idraqam = idraqam
+# #         self.bosqich = 1
+# #         self.manzil = manzil
+# #
+# #     def get_id(self):
+# #         """Talabaning id raqamini"""
+# #         return self.idraqam
+# #
+# #     def get_bosqich(self):
+# #         """Talabaning id raqamini"""
+# #         return self.bosqich
+# #
+# #     def get_info(self):
+# #         """Talaba haqwida ma'lumot"""
+# #         info = f"{self.ism} {self.familiya}."
+# #         info += f"{self.get_bosqich()}-bosqich. ID raqami:{self.idraqam}"
+# #         return info
+# # #
+# # # talaba = Talaba("Og'abek", "Abdiyev", "FB115522",2000,"1122008")
+# # # print(talaba.get_info())
+# # # print(talaba.get_id())
+# # # print(talaba.get_bosqich())
+# # class Manzil:
+# #     """manzil saqlash uchun klass"""
+# #     def __init__(self,uy,kocha,tuman,viloyat):
+# #         """Manzil xususiyatlari"""
+# #         self.uy = uy
+# #         self.kocha = kocha
+# #         self.tuman = tuman
+# #         self.viloyat = viloyat
+# #
+# #     def get_manzil(self):
+# #         """Manzilni ko'rish"""
+# #         manzil = f"{self.viloyat} viloyat, {self.tuman} tumani,"
+# #         manzil +=f"{self.kocha} ko'chasi, {self.uy}-uy"
+# #
+# # talaba_manzil = Manzil(33,'alpomish','urganch','xorazm')
+# # talaba = Talaba("Otabek","Ro'zmetov","FA112299",2008,"0000012",talaba_manzil)
+# #
+# #
+# # print(talaba.manzil.get_manzil())
+#
 # class Shaxs:
-#     """Sahxslarni haqida ma'lumot"""
-#     def __init__(self,ism,familiya,passport,tyil):
+#     """Shaxslar haqida ma'lumot"""
+#
+#     def __init__(self, ism, familiya, passport, tyil):
 #         self.ism = ism
 #         self.familiya = familiya
 #         self.passport = passport
@@ -8,47 +77,43 @@
 #
 #     def get_info(self):
 #         """Shaxs haqida ma'lumot"""
-#         info = f"{self.ism} {self.familiya}."
-#         info += f"Passport.{self.passport},{self.tyil}-yilda tug'ilgan"
+#         info = f"{self.ism} {self.familiya}. "
+#         info += f"Passport:{self.passport}, {self.tyil}-yilda tug`ilgan"
 #         return info
 #
-#     def get_age(self,yil):
+#     def get_age(self, yil):
 #         """Shaxsning yoshini qaytaruvchi metod"""
 #         return yil - self.tyil
 #
-# # inson = Shaxs("Otabek","Ro'zmetov","FB1122",2008)
-# # print(f"{inson.get_info()}. {inson.get_age(2023)} yoshda")
 #
 # class Talaba(Shaxs):
-#     """talaba Klassi"""
-#     def __init__(self,ism,familiya,passport,tyil,idraqam,manzil):
+#     """Talaba klassi"""
+#     def __init__(self, ism, familiya, passport, tyil, idraqam, manzil):
 #         """Talabaning xususiyatlari"""
-#         super().__init__(ism,familiya,passport,tyil)
+#         super().__init__(ism, familiya, passport, tyil)
 #         self.idraqam = idraqam
 #         self.bosqich = 1
-#         self.manzil = manzil
+#         self.manzil=manzil
 #
 #     def get_id(self):
-#         """Talabaning id raqamini"""
+#         """Talabaning ID raqami"""
 #         return self.idraqam
 #
 #     def get_bosqich(self):
-#         """Talabaning id raqamini"""
+#         """Talabaning o'qish bosqichi"""
 #         return self.bosqich
 #
 #     def get_info(self):
-#         """Talaba haqwida ma'lumot"""
-#         info = f"{self.ism} {self.familiya}."
-#         info += f"{self.get_bosqich()}-bosqich. ID raqami:{self.idraqam}"
+#         """Talaba haqida ma'lumot"""
+#         info = f"{self.ism} {self.familiya}. "
+#         info += f"{self.get_bosqich()}-bosqich. ID raqami: {self.idraqam}"
 #         return info
-# #
-# # talaba = Talaba("Og'abek", "Abdiyev", "FB115522",2000,"1122008")
-# # print(talaba.get_info())
-# # print(talaba.get_id())
-# # print(talaba.get_bosqich())
+#
+#
 # class Manzil:
-#     """manzil saqlash uchun klass"""
-#     def __init__(self,uy,kocha,tuman,viloyat):
+#     """Manzil saqlash uchun klass"""
+#
+#     def __init__(self, uy, kocha, tuman, viloyat):
 #         """Manzil xususiyatlari"""
 #         self.uy = uy
 #         self.kocha = kocha
@@ -57,77 +122,12 @@
 #
 #     def get_manzil(self):
 #         """Manzilni ko'rish"""
-#         manzil = f"{self.viloyat} viloyat, {self.tuman} tumani,"
-#         manzil +=f"{self.kocha} ko'chasi, {self.uy}-uy"
+#         manzil = f"{self.viloyat} viloyati, {self.tuman} tumani, "
+#         manzil += f"{self.kocha} ko'chasi, {self.uy}-uy"
+#         return manzil
 #
-# talaba_manzil = Manzil(33,'alpomish','urganch','xorazm')
-# talaba = Talaba("Otabek","Ro'zmetov","FA112299",2008,"0000012",talaba_manzil)
+# talaba_manzil = Manzil(12,'Olmazor',"Bog'bon","Samarqand")
+# talaba = Talaba("Valijon","Aliyev","FA112299",2000,"0000012",talaba_manzil)
 #
 #
 # print(talaba.manzil.get_manzil())
-
-class Shaxs:
-    """Shaxslar haqida ma'lumot"""
-
-    def __init__(self, ism, familiya, passport, tyil):
-        self.ism = ism
-        self.familiya = familiya
-        self.passport = passport
-        self.tyil = tyil
-
-    def get_info(self):
-        """Shaxs haqida ma'lumot"""
-        info = f"{self.ism} {self.familiya}. "
-        info += f"Passport:{self.passport}, {self.tyil}-yilda tug`ilgan"
-        return info
-
-    def get_age(self, yil):
-        """Shaxsning yoshini qaytaruvchi metod"""
-        return yil - self.tyil
-
-
-class Talaba(Shaxs):
-    """Talaba klassi"""
-    def __init__(self, ism, familiya, passport, tyil, idraqam, manzil):
-        """Talabaning xususiyatlari"""
-        super().__init__(ism, familiya, passport, tyil)
-        self.idraqam = idraqam
-        self.bosqich = 1
-        self.manzil=manzil
-
-    def get_id(self):
-        """Talabaning ID raqami"""
-        return self.idraqam
-
-    def get_bosqich(self):
-        """Talabaning o'qish bosqichi"""
-        return self.bosqich
-
-    def get_info(self):
-        """Talaba haqida ma'lumot"""
-        info = f"{self.ism} {self.familiya}. "
-        info += f"{self.get_bosqich()}-bosqich. ID raqami: {self.idraqam}"
-        return info
-
-
-class Manzil:
-    """Manzil saqlash uchun klass"""
-
-    def __init__(self, uy, kocha, tuman, viloyat):
-        """Manzil xususiyatlari"""
-        self.uy = uy
-        self.kocha = kocha
-        self.tuman = tuman
-        self.viloyat = viloyat
-
-    def get_manzil(self):
-        """Manzilni ko'rish"""
-        manzil = f"{self.viloyat} viloyati, {self.tuman} tumani, "
-        manzil += f"{self.kocha} ko'chasi, {self.uy}-uy"
-        return manzil
-
-talaba_manzil = Manzil(12,'Olmazor',"Bog'bon","Samarqand")
-talaba = Talaba("Valijon","Aliyev","FA112299",2000,"0000012",talaba_manzil)
-
-
-print(talaba.manzil.get_manzil())
--- uyga vazifalar.py
+++ uyga vazifalar.py
@@ -1,64 +1,64 @@
-# 'UY ISHI'
-# '16-DARS FUNKSIYA'
-# ('uyga vazifa sharti:"1.Foydalanuvchidan son olib, '
-#  'uning kvadrati va kubini '
-#  'konsolga chiqaruvchi funksiya yozing.')
-#
-# def Foydalanuvchi():
-#     """Kvadrat va kubni konsolga chiqaruvchi funksiya"""
-#     print(Foydalanuvchi.__doc__)
-#     square = int(input("Hohlagan natural son yozing>>"))
-#     print(f"{square} ning kavadrati, = {square,  2} ga teng")
-#     print(f"{square} ning kubi, = {square,  3} ga teng")
-#
-# 'UY ISHI'
-# "18-DARS FUNKSIYA VA RO'HAT"
-# ("uyga vazifa sharti:Funkiyaga ro'yhat uzatib ro'yhatda "
-#  "uchragan vali ismini birinchi harfini katta harf bilan,"
-# "qolgan elementlarini hamma harfini katta harf bilan "
-#  "chiqaring.Hamda ali ismli odamga salom yollang!")
-#
-#
-# def katta_harf(matnlar):
-#  for i in range(len(matnlar)):
-#   matnlar[i] = matnlar[i].upper()
-#   matnlar[1] = matnlar[1].title()
-#
-# ismlar = ['ali', 'vali', 'hasan', 'husan']
-# katta_harf(ismlar)
-# print(ismlar)
-# print('salom ali'.title())
-#
-# def katta_harf(matnlar):
-#     for i in range(len(matnlar)):
-#         if matnlar[i] == 'ali':
-#             matnlar[i] = matnlar[0].title()
-#         else:
-#             matnlar[i] = matnlar[i].upper()
-#
-#
-# ismlar = ['ali', 'vali', 'hasan', 'husan']
-# katta_harf(ismlar)
-# print(ismlar)
-#
-# 'UY ISHI'
-# '19-DARS Moslashuvchan funksiya'
-# ('uyga vazifa sharti:"1.Istalgancha sonlarni qabul qilib, ularning'
-#   "ko'paytmasini qaytaruvchi funksiya yozing")
-#
-# def sonlarni_qaytaravchi_funk(*sonlar):
-#     """istalgan sonni qabul qilib ularning ko'paytmasini qaytaruvchi funksiya"""
-#     yigindi = 1
-#     for son in sonlar:
-#         yigindi *= son
-#     return yigindi
-#
-# print(sonlarni_qaytaravchi_funk.__doc__)
-# print(sonlarni_qaytaravchi_funk(4, 5,6,2 ))
-# print(sonlarni_qaytaravchi_funk(2,6))
-#
-#
-#
+'UY ISHI'
+'16-DARS FUNKSIYA'
+('uyga vazifa sharti:"1.Foydalanuvchidan son olib, '
+ 'uning kvadrati va kubini '
+ 'konsolga chiqaruvchi funksiya yozing.')
+
+def Foydalanuvchi():
+    """Kvadrat va kubni konsolga chiqaruvchi funksiya"""
+    print(Foydalanuvchi.__doc__)
+    square = int(input("Hohlagan natural son yozing>>"))
+    print(f"{square} ning kavadrati, = {square,  2} ga teng")
+    print(f"{square} ning kubi, = {square,  3} ga teng")
+
+'UY ISHI'
+"18-DARS FUNKSIYA VA RO'HAT"
+("uyga vazifa sharti:Funkiyaga ro'yhat uzatib ro'yhatda "
+ "uchragan vali ismini birinchi harfini katta harf bilan,"
+"qolgan elementlarini hamma harfini katta harf bilan "
+ "chiqaring.Hamda ali ismli odamga salom yollang!")
+
+
+def katta_harf(matnlar):
+ for i in range(len(matnlar)):
+  matnlar[i] = matnlar[i].upper()
+  matnlar[1] = matnlar[1].title()
+
+ismlar = ['ali', 'vali', 'hasan', 'husan']
+katta_harf(ismlar)
+print(ismlar)
+print('salom ali'.title())
+
+def katta_harf(matnlar):
+    for i in range(len(matnlar)):
+        if matnlar[i] == 'ali':
+            matnlar[i] = matnlar[0].title()
+        else:
+            matnlar[i] = matnlar[i].upper()
+
+
+ismlar = ['ali', 'vali', 'hasan', 'husan']
+katta_harf(ismlar)
+print(ismlar)
+
+'UY ISHI'
+'19-DARS Moslashuvchan funksiya'
+('uyga vazifa sharti:"1.Istalgancha sonlarni qabul qilib, ularning'
+  "ko'paytmasini qaytaruvchi funksiya yozing")
+
+def sonlarni_qaytaravchi_funk(*sonlar):
+    """istalgan sonni qabul qilib ularning ko'paytmasini qaytaruvchi funksiya"""
+    yigindi = 1
+    for son in sonlar:
+        yigindi *= son
+    return yigindi
+
+print(sonlarni_qaytaravchi_funk.__doc__)
+print(sonlarni_qaytaravchi_funk(4, 5,6,2 ))
+print(sonlarni_qaytaravchi_funk(2,6))
+
+
+
 class Avto:
     def __init__(self,rangi,nomi,model,narh,):
         self.rangi =rangi
@@ -101,6 +101,42 @@ avto1.update_km()
 print(avto1.get_info())
 avto1.update_km()
 print(avto1.get_info())
+
+
+
+"UY ISHI"
+"""26-DARS UYGA VAZIFA SHARTI:"""
+
+class Foydalanuvchi:
+    def __init__(self,ism,familiya,tyil,username):
+        """Foydalanuvchi haqida malumot"""
+        self.ism = ism
+        self.familiya = familiya
+        self.tyil = tyil
+        self.username = username
+
+    def get_fullname(self):
+        """Foydalanuvchi haqida malumoti"""
+        fullname = f"{self.ism} {self.familiya},"
+        fullname += f"{self.tyil} {self.username}"
+        return fullname
+
+    def age_yil(self,yil):
+        """Foydalanuvchini yoshini qaytaruvchi metod"""
+        return yil - self.tyil
+
+user = Foydalanuvchi("Otabek","Ro'zmetov",2008,"otabek12")
+print(f"{user.get_fullname()}, {user.age_yil(2023)} yoshda")
+
+class Admin(Foydalanuvchi):
+    """admin voris class"""
+    def __init__(self,ism,familiya,tyil,username):
+        """Admin xususiyatlari"""
+        super().__init__(self,ism,familiya,tyil,username)
+
+    def ban_user(self,user):
+        """user"""
+print(f"Foydalanuvchi {user.ism} {user.familiya} bloklandi")
 
 
 
